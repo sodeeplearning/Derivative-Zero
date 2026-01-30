27244988a4731d7b391b2b4eb41afc784d692796
Vitallya <vitaliy.petreev@gmail.com>
2026-01-30 06:04:16 +0300

    Added translate_text method to AIClient + cosmetic changes (safe_call decorator)

--- app/core/ai_client.py
+++ app/core/ai_client.py
@@ -2,15 +2,42 @@ import requests
 import base64
 import warnings
 
+from typing import Callable
+
 
 class AIClientError(Exception):
     ...
+
+
+def safe_request(func: Callable):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+
+        except requests.exceptions.Timeout:
+            raise AIClientError("⏱ Сервер не отвечает (timeout)")
+        except requests.exceptions.ConnectionError:
+            raise AIClientError("🔌 Не удалось подключиться к серверу")
+        except requests.exceptions.HTTPError as e:
+            raise AIClientError(
+                f"❌ Ошибка сервера: {e.response.status_code}"
+            )
+        except ValueError:
+            raise AIClientError("📄 Сервер вернул не JSON")
+        except Exception as e:
+            raise AIClientError(f"💥 Неизвестная ошибка: {e}")
+
+    return wrapper
 
 
 class AIClient:
     def __init__(self, url):
         self.url = url
 
+    def set_url(self, url):
+        self.url = url
+
+    @safe_request
     def ask(self, page_text, images, question) -> str:
         payload = {
             "context": page_text,
@@ -21,35 +48,14 @@ class AIClient:
             ]
         }
 
-        try:
-            response = requests.post(
-                self.url + "/ai-consulter/sync",
-                json=payload,
-                timeout=120,
-            )
-            response.raise_for_status()
-            data = response.json()
-            return data["text"]
-
-        except requests.exceptions.Timeout:
-            raise AIClientError("⏱ Сервер не отвечает (timeout)")
-
-        except requests.exceptions.ConnectionError:
-            raise AIClientError("🔌 Не удалось подключиться к серверу")
-
-        except requests.exceptions.HTTPError as e:
-            raise AIClientError(
-                f"❌ Ошибка сервера: {e.response.status_code}"
-            )
-
-        except ValueError:
-            raise AIClientError("📄 Сервер вернул не JSON")
-
-        except Exception as e:
-            raise AIClientError(f"💥 Неизвестная ошибка: {e}")
-
-    def set_url(self, url):
-        self.url = url
+        response = requests.post(
+            self.url + "/ai-consulter/sync",
+            json=payload,
+            timeout=120,
+        )
+        response.raise_for_status()
+        data = response.json()
+        return data["text"]
 
     def clear_chat_history_no_exceptions(self):
         try:
@@ -60,23 +66,15 @@ class AIClient:
         except Exception as e:
             warnings.warn(f"Trying to update chat history caused error: {e}")
 
+    @safe_request
     def clear_chat_history(self):
-        try:
-            response = requests.delete(
-                self.url + "/ai-consulter/clear_chat_history",
-                timeout=40,
-            )
-            response.raise_for_status()
+        response = requests.delete(
+            self.url + "/ai-consulter/clear_chat_history",
+            timeout=40,
+        )
+        response.raise_for_status()
 
-        except requests.exceptions.Timeout:
-            raise AIClientError("⏱ Сервер не отвечает (timeout)")
-
-        except requests.exceptions.ConnectionError:
-            raise AIClientError("🔌 Не удалось подключиться к серверу")
-
-        except Exception as e:
-            raise AIClientError(f"💥 Неизвестная ошибка: {e}")
-
+    @safe_request
     def get_speech(
             self,
             texts: str | list[str],
@@ -89,29 +87,29 @@ class AIClient:
             "texts": texts,
             "voice": voice,
         }
-        try:
-            response = requests.post(
-                self.url + "/tts/async",
-                json=payload,
-                timeout=600,
-            )
-            response.raise_for_status()
-            data = response.json()
-            return data["audio_base64"]
 
-        except requests.exceptions.Timeout:
-            raise AIClientError("⏱ Сервер не отвечает (timeout)")
+        response = requests.post(
+            self.url + "/tts/async",
+            json=payload,
+            timeout=600,
+        )
+        response.raise_for_status()
+        data = response.json()
 
-        except requests.exceptions.ConnectionError:
-            raise AIClientError("🔌 Не удалось подключиться к серверу")
+        return data["audio_base64"]
 
-        except requests.exceptions.HTTPError as e:
-            raise AIClientError(
-                f"❌ Ошибка сервера: {e.response.status_code}"
-            )
+    @safe_request
+    def translate_text(self, text: str) -> str:
+        payload = {
+            "text": text,
+        }
 
-        except ValueError:
-            raise AIClientError("📄 Сервер вернул не JSON")
+        response = requests.post(
+            self.url + "/translator/async",
+            json=payload,
+            timeout=200,
+        )
+        response.raise_for_status()
+        data = response.json()
 
-        except Exception as e:
-            raise AIClientError(f"💥 Неизвестная ошибка: {e}")
+        return data["text"]
